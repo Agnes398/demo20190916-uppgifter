2ff742bb4c7191625419fecad40052ed109933c0
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-16 10:44:25 +0200

    Uppgift 2.2 klar, enkel lösning

--- Uppgift 1/Uppgift 2/Program.cs
+++ Uppgift 1/Uppgift 2/Program.cs
@@ -13,40 +13,17 @@ namespace Uppgift_2
             Console.WriteLine("Please enter a sentance: ");
             string UserResponse = Console.ReadLine();
 
-            int wordcount = 1; //dvs när det första spacet hittas är det ord 2
-            foreach (char c in UserResponse)
-            {
-                if (c == ' ')
-                {
-                    wordcount++;
-                }
-            }
 
-            string[] words = new string[10];
-            string word = "";
-            int i = 0;
-            foreach (char c in UserResponse)
-            {
-                if (c != ' ')
-                {
-                    word = word + c;
-                }
-                else
-                {
-                    words[i] = word;
-                    word = "";
-                    i++;
-                }
-                
-            }
+            string[] words = UserResponse.Split(' ');
 
             Console.WriteLine("Words består av {0} ord", words.Length);
-            Console.WriteLine(words);
 
             foreach (string s in words)
             {
                 Console.WriteLine("{0} ({1})", s, s.Length);
             }
+
+            
 
         }
     }
