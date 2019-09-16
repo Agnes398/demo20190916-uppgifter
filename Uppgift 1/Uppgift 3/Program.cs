eede0b27358cd490b6903bf073b92dbe8afde164
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-16 14:08:14 +0200

    new project, uppgift 3

---
+++ Uppgift 1/Uppgift 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening: ");
+            string mening = Console.ReadLine();
+
+            //Dela upp meningen i ord
+            string[] ord = mening.Split(' ');
+
+            foreach (string s in ord)
+            {
+
+            }
+
+        }
+    }
+}
