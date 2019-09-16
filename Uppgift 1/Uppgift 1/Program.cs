160307571f7976f7ef8d659f7a43c797f71e10a5
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-16 10:01:01 +0200

    Uppdated ver of previous commit

--- Uppgift 1/Uppgift 1/Program.cs
+++ Uppgift 1/Uppgift 1/Program.cs
@@ -10,24 +10,13 @@ namespace Uppgift_1
     {
         static void Main(string[] args)
         {
+            //Uppgift 1.1
             Console.WriteLine("Please enter a line of text: ");
             String userResponse = Console.ReadLine();
-
-            Console.WriteLine();
             Console.WriteLine("Thank you for entering: '{0}' ", userResponse);
 
-
+            //Uppgift 1.2
             int wordcount = 1; //dvs när det första spacet hittas är det ord 2
-            //for (int i = 0; i < userResponse.Length; i++)
-            //{
-            //    if (userResponse[i] == ' ')
-            //    {
-            //        wordcount++;
-            //    }
-            //}
-
-            Console.WriteLine("Din mening har {0} ord.", wordcount);
-
             foreach (char c in userResponse)
             {
                 if (c == ' ')
