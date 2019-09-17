674cbe14b2f42b43db75ee97e5c61dbedb34ed78
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-17 23:22:21 +0200

    Uppdates and cleanup on uppgift 1

--- Uppgift 1/Uppgift 1/Program.cs
+++ Uppgift 1/Uppgift 1/Program.cs
@@ -11,9 +11,9 @@ namespace Uppgift_1
         static void Main(string[] args)
         {
             //Uppgift 1.1
-            Console.WriteLine("Please enter a line of text: ");
+            Console.WriteLine("Var snäll och mata in en mening: ");
             String userResponse = Console.ReadLine();
-            Console.WriteLine("Thank you for entering: '{0}' ", userResponse);
+            Console.WriteLine("Tack för att du matade in: '{0}' ", userResponse);
 
             //Uppgift 1.2
             int wordcount = 1; //dvs när det första spacet hittas är det ord 2
@@ -28,19 +28,15 @@ namespace Uppgift_1
             Console.WriteLine("Din mening har {0} ord.", wordcount);
 
             //Uppgift 1.3
-            Console.WriteLine("Please enter a sentance: ");
+            Console.WriteLine("Mata in en mening: ");
             string UserResponse = Console.ReadLine();
 
             string[] words = UserResponse.Split(' ');
-
-            Console.WriteLine("Words består av {0} ord", words.Length);
 
             foreach (string s in words)
             {
                 Console.WriteLine("{0} ({1})", s, s.Length);
             }
-
-
 
         }
     }
